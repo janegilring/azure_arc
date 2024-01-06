71f317ae2f7aad97771c03517fbab8a0bf1617b3
Jan Egil Ring <janegilring@microsoft.com>
2024-01-06 06:24:43 +0000

    Update Azure login credentials in ArcServersLogonScript.ps1
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_jumpstart_arcbox/artifacts/ArcServersLogonScript.ps1
+++ azure_jumpstart_arcbox/artifacts/ArcServersLogonScript.ps1
@@ -188,7 +188,7 @@ if ($Env:flavor -ne "DevOps") {
     # Register Azure providers
     Write-Header "Registering Providers"
     @("Microsoft.HybridCompute","Microsoft.HybridConnectivity","Microsoft.GuestConfiguration","Microsoft.AzureArcData") | ForEach-Object -Parallel {
-        $null = az login --service-principal --username $env:spnClientId --password $env:ClientSecret --tenant $env:TenantId
+        $null = az login --service-principal --username $env:spnClientId --password $env:spnClientSecret --tenant $env:spnTenantId
         az provider register --namespace $PSItem --wait --only-show-errors
     }
 
