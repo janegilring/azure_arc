e18c9d222ec9a23fc5d4fc54fe55d64f8d05c018
Jan Egil Ring <janegilring@microsoft.com>
2025-10-26 20:27:52 +0100

    Specify versions for WinGet PowerShell modules in WinGet.ps1
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_jumpstart_localbox/artifacts/PowerShell/WinGet.ps1
+++ azure_jumpstart_localbox/artifacts/PowerShell/WinGet.ps1
@@ -15,8 +15,8 @@ Connect-AzAccount -Identity -Tenant $Env:tenantId -Subscription $Env:subscriptio
 Update-AzDeploymentProgressTag -ProgressString 'Installing WinGet packages...' -ResourceGroupName $env:resourceGroup -ComputerName $env:computername
 
 # Install WinGet PowerShell modules
-Install-PSResource -Name Microsoft.WinGet.Client -Scope AllUsers -Quiet -AcceptLicense -TrustRepository
-Install-PSResource -Name Microsoft.WinGet.DSC -Scope AllUsers -Quiet -AcceptLicense -TrustRepository
+Install-PSResource -Name Microsoft.WinGet.Client -Scope AllUsers -Quiet -AcceptLicense -TrustRepository -Version 1.11.460
+Install-PSResource -Name Microsoft.WinGet.DSC -Scope AllUsers -Quiet -AcceptLicense -TrustRepository -Version 1.11.460
 
 # Install DSC resources required for ArcBox
 Install-PSResource -Name DSCR_Font -Scope AllUsers -Quiet -AcceptLicense -TrustRepository
